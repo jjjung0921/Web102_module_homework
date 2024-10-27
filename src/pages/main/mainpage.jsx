8efa9e8aef93b04854b0f0b5f728f36cfe2f7eda
jjjung0921 <earthenergy-1128@naver.com>
2024-10-27 16:28:11 +0900

    mainpage topbar 작성

---
+++ src/pages/main/mainpage.jsx
@@ -0,0 +1,17 @@
+import logo from "../../assets/logo.png";
+
+Phrase = ["How it works", "Gifts", "About", "logo", "Login", "Members", "Contracts"];
+
+const element = (phrase) => {
+  
+    return(
+        <span>
+            {phrase}
+        </span>
+    );
+};
+
+export const Header = 
+<div id="Header">
+
+</div>;

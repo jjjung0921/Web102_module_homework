80f9834342395332106f834e37afdcc5a863a6d2
jjjung0921 <earthenergy-1128@naver.com>
2024-10-28 18:18:44 +0900

    [ci run] CI 시간 지정

--- src/components/main/main_picture.jsx
+++ src/components/main/main_picture.jsx
@@ -2,7 +2,7 @@ import { image } from "../../image";
 
 export const Main_image = () =>{
     return(
-        <img src = {image('main')} id="main_picture"/>
+        <img src = {image('main')} id="main_picture" alt="main_picture"/>
     );
 };
     
